4dde01e4ab56824dd6e914a45beb3ec078066ba1
11atiasbernal <matias.bernal.quintero@hotmail.com>
2024-12-12 17:26:08 -0500

    Update clase.py

--- clase.py
+++ clase.py
@@ -1,1 +1,13 @@
+#gracias
 
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[word])
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
+    print("no tenemos esta palabra en este diccionario:(")
